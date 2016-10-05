83683dfa072558a4d41c84ce7fde58eefef1c199
perecastors <58650@supinfo.com>
2016-10-05 09:41:27 +0200

    1er checkin

---
+++ src/com/formation/Bonjour.java
@@ -0,0 +1,8 @@
+package com.formation;
+
+public class Bonjour {
+
+    public String yooo(String name){
+        return "yooooo " + name;
+    }
+}
---
+++ src/com/formation/Lanceur.java
@@ -0,0 +1,12 @@
+package com.formation;
+
+/**
+ * Created by Formation on 05/10/2016.
+ */
+public class Lanceur {
+
+    public static void main(String[] args) {
+        Bonjour bj = new Bonjour();
+        System.out.println(bj.yooo("Rasta"));
+        }
+}
